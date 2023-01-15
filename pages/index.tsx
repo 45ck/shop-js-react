528ed8adab53ff9e67eb2a545187d68f8bc52640
45ck <98618920+45ck@users.noreply.github.com>
2023-01-15 21:02:24 +1100

    Documented changes to front page

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,8 @@ import { Item, Proprietor } from '../database/types'
 
 const inter = Inter({ subsets: ['latin'] })
 
+// key value pair to find the owner of an item
+
 interface OwnerItem {
   proprietor: Proprietor,
   item: Item
@@ -14,16 +16,20 @@ interface OwnerItem {
 
 export default function FrontPage() {
 
-
   const [displayItems, setDisplayItems] = useState<Item[]>([]);
   const [proprietors, setProprietors] = useState<OwnerItem[]>([])
   const hasFetchedPages: MutableRefObject<boolean> = useRef(false);
 
   useEffect(() => {
-    if (hasFetchedPages.current) return;
 
+    // makes sure that useEffect only runs once - it runs twice since strict mode is off. 
+
+    if (hasFetchedPages.current) return;
     hasFetchedPages.current = true;
-    axios.get("/api/get_items_page?page=2").then((res: AxiosResponse<any, any>) => {
+
+    // retrieve items from first page
+
+    axios.get("/api/get_items_page?page=1").then((res: AxiosResponse<any, any>) => {
       res.data.sqlItems.forEach((sqlItem: any) => {
 
         let item: Item = new Item(sqlItem);
@@ -34,14 +40,17 @@ export default function FrontPage() {
 
           let proprietor: Proprietor = new Proprietor(resOwner.data.result[0]);
 
-          console.log({ proprietor: proprietor, item: item })
+          // add to key value pair list of items and proprietors to later in rendering map items to there owners
 
           setProprietors(value => [...value, { proprietor: proprietor, item: item }])
 
         });
 
+        // don't add the item twice 
+
         if (!displayItems.includes(item))
           setDisplayItems(value => [item, ...value]);
+          
       });
     })
   }, []);
