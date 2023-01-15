c53ec75cdec0b5a9472a5eacedf74fe332be8ee3
45ck <98618920+45ck@users.noreply.github.com>
2023-01-15 12:49:37 +1100

    Updated items prototype
     Updated items prototype to include displaying proprietors name when displaying the item using a dynamic URL.

---
+++ database/types.tsx
@@ -0,0 +1,36 @@
+export class Item {
+    id: number;
+    name: string;
+    price: number;
+    owner: number;
+    availableStock: number;
+    purchaseable: boolean;
+    description: string;
+
+    constructor(object: any) {
+        this.id = object.item_id;
+        this.name = object.item_name;
+        this.price = object.item_price;
+        this.owner = object.item_owner;
+        this.availableStock = object.item_available_stock;
+        this.purchaseable = object.item_purchaseable;
+        this.description = object.item_description;
+        console.log(this);
+    }
+}
+
+export class Proprietor {
+    id: number;
+    name: string;
+
+    constructor(object: any) {
+        this.id = object.proprietor_id;
+        this.name = object.proprietor_name;
+        console.log(this);
+    }
+}
+
+
+
+
+
--- pages/items/[id]/index.tsx
+++ pages/items/[id]/index.tsx
@@ -2,44 +2,62 @@ import Head from 'next/head'
 import { useEffect, useLayoutEffect, useState } from 'react'
 import axios, { AxiosResponse } from 'axios'
 import { useRouter } from 'next/router'
+import { Atkinson_Hyperlegible } from '@next/font/google'
+import { NumericLiteral } from 'typescript'
+import { Item, Proprietor  } from '../../../database/types';
 
 export default function GetItem() {
     const router = useRouter()
 
     const { query } = router;
 
-    interface Item {
-        item_id: number,
-        item_name: string,
-        item_price: number,
-        item_owner: number,
-        item_available_stock: number,
-        item_purchaseable: boolean,
-        item_description: string
-    }
-    
     const [itemData, setItemData] = useState<Item>();
+
+    const [proprietorData, setProprietorData] = useState<Proprietor>();
+
+    const [isRealItem, setIsRealItem] = useState<boolean>(true);
 
     useEffect(() => {
         if (!query.id)
             return;
 
-        console.log(`/api/get_item?id=${query.id}`)
+        // retrieve item data from our api
+
         axios.get(`/api/get_item?id=${query.id}`).then((res: AxiosResponse<any, any>) => {
             if (res.data.result.length == 0) {
-                
+                setIsRealItem(false);
             } else {
-                setItemData(res.data.result[0]);
+                setItemData(new Item(res.data.result[0]));
             }
         });
-      }, [query.id]);
-  
-        return (
-            <div>
-                <h1> {itemData?.item_name} </h1>
-                <h2> {itemData?.item_owner} - ${itemData?.item_price} </h2>
-                <p> {itemData?.item_description} </p>
-                
-            </div>
-        )
-  }
+            
+    }, [query.id]);
+
+    useEffect(() => {
+
+        // retrieve data about the owner of the item
+
+        if (itemData?.owner != undefined)
+            axios.get(`/api/get_entity?type=proprietors&id=${itemData?.owner}`).then((res: AxiosResponse<any, any>) => {
+                if (res.data.result.length != 0) 
+                    setProprietorData(new Proprietor(res.data.result[0]));
+            });
+
+        
+    }, [itemData])
+
+    return (
+        <>
+            {!isRealItem && <div> <h1> This page does not exist. </h1></div>}
+
+            {isRealItem && 
+                <div>
+                    <h1> {itemData?.name} </h1>
+                    <h2> {itemData?.owner} - ${itemData?.price} - {proprietorData?.name} </h2>
+                    <p> {itemData?.description} </p>
+
+                </div>
+            }
+        </>
+    )
+}
