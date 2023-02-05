ef9156bb5346761e0bb5fdd7fbd3c52871ab202f
45ck <98618920+45ck@users.noreply.github.com>
2023-02-05 15:29:01 +1100

    Refined the visual appearance of the AddToCartButton component

--- components/AddToCartButton.tsx
+++ components/AddToCartButton.tsx
@@ -10,8 +10,8 @@ export default function AddToCartButton(prop: { item: Item }) {
   return (
     <>
       <div>
-          <div className=" border-solid border-y-2 border-l-2 w-fit h-fit p-2 inline rounded-l-lg border-slate-600 bg-slate-500/25 "> {userData?.get.cart.filter((item) => { return item.id == prop.item?.id }).length} </div>
-            <button className="border-solid border-2 w-fit h-fit p-2 m-0 inline rounded-sm rounded-r-lg border-slate-600 bg-slate-500/25 leading-[initial] hover:bg-slate-500 hover:text-slate-50 active:bg-slate-700 transition-colors " onClick={() => { if (prop.item != undefined) userData?.set({ cart: [...userData.get.cart, prop.item] }) }}> + </button>
+          <div className=" border-solid border-y-2 border-l-2 w-fit h-fit p-2 inline rounded-l-lg border-slate-600 bg-slate-300 "> {userData?.get.cart.filter((item) => { return item.id == prop.item?.id }).length} </div>
+            <button className="border-solid border-2 w-fit h-fit p-2 m-0 inline rounded-sm rounded-r-lg border-slate-600 bg-slate-300 leading-[initial] hover:bg-slate-400 hover:text-slate-50 active:bg-slate-700 transition-colors " onClick={() => { if (prop.item != undefined) userData?.set({ cart: [...userData.get.cart, prop.item] }) }}> + </button>
       </div>
     </>
   )
