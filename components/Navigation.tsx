f8b4a6c0f927a2edc156ba4e6e8f209afc8d9e5e
45ck <98618920+45ck@users.noreply.github.com>
2023-02-10 15:58:28 +1100

    Refactored search submit handling default behaviour to preserve global user state

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import React, { useContext, useEffect, useRef, useState } from 'react'
+import React, { FormEvent, FormEventHandler, useContext, useEffect, useRef, useState } from 'react'
 import { UserDataContext } from '../pages/_app'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCartShopping, faUser } from '@fortawesome/free-solid-svg-icons';
@@ -11,16 +11,18 @@ import { useRouter } from 'next/router';
 
 export default function Navigation() {
 
+  const router = useRouter();
+
   library.add(faCartShopping, faUser);
 
   const userData = useContext(UserDataContext);
 
   const [categories, setCategories] = useState<Category[]>([]);
+  
 
   // retrieve a list of all categories 
 
   useEffect(() => {
-    console.log("START")
 
     axios.get("/api/get_entity?type=categories").then((value: AxiosResponse<any, any>) => {
 
@@ -28,25 +30,32 @@ export default function Navigation() {
         console.log(categories.length)
         setCategories(currentCategory => currentCategory = [...currentCategory, new Category(value)])
       });
-
-      
-    })
-
-    return () => console.log("END");
+    });
   }, []);
 
-  useEffect(() => {
-    console.log(categories.length, categories)
-  }, [categories])
+  const onSubmitQuery = (event: FormEvent) => {
+    event.preventDefault();
+    
+    // retrieve value from search input element
+
+    let query =  (event.currentTarget.children[1] as HTMLInputElement).value;
+
+    console.log(query)
+
+    // go to page where search query will be retrieve and than rendered accordingly 
+
+    router.push(`/search?q=${query}`, undefined, { shallow: false });
+
+  }
 
   return (
     <>
       <nav className=" w-screen h-fit bg-neutral-200 ">
         <div className="w-100 flex justify-between items-center py-2 pl-2 pr-8">
           <Link href="/" className=' text-2xl font-bold uppercase font-noto '>Home</Link>
-          <form action='/search' className=' border-solid border-2 border-slate-700 rounded-lg'>
+          <form onSubmit={onSubmitQuery} className=' border-solid border-2 border-slate-700 rounded-lg'>
             <label className=' border-r-2 border-slate-700 border-solid p-2 font-viga'> Search </label>
-            <input type={"text"} name="q" className="lg:w-52 p-2 font-viga font-light focus-visible:outline-none " />
+            <input type={"text"} name="q" className="lg:w-52 p-2 font-viga font-light focus-visible:outline-none" />
           </form>
           <div className='flex'>
             <Link href="/account" className='mr-2 bg-slate-400 flex items-center py-0 px-2 rounded-lg'> <FontAwesomeIcon icon="user" size="1x" className='mr-2 fill-slate-800' color='var(--zinc-black)' /> <span className='font-viga'> {userData?.get.account ? userData.get.account.email : "Sign in" } </span> </Link>
--- pages/search.tsx
+++ pages/search.tsx
@@ -1,11 +1,9 @@
 import Head from 'next/head'
-import { MutableRefObject, useContext, useEffect, useRef, useState } from 'react'
+import {useEffect, useState } from 'react'
 import axios, { Axios, AxiosResponse } from 'axios'
 import { Item, OwnerItem, Picture, Proprietor } from '../database/types'
 import RowItem from '../components/RowItem'
-import { UserDataContext } from './_app'
-import Link from 'next/link'
-import { createSolutionBuilder } from 'typescript'
+
 import { useRouter } from 'next/router'
 
 
